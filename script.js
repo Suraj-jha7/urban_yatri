dbc2d6991341aa173d3607b12594db57f3c90491
Suraj-jha7 <jhasuraj7381@gmail.com>
2024-02-09 19:11:33 +0530

    slider added

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+
+let outerContainer=document.querySelector(".slider_container");
+let inner_slider=document.querySelector(".inner_slider");
+
+let pressed=false;
+let startx;
+let  x;
+
+
+outerContainer.addEventListener('mousedown',(e)=>{
+  pressed=true;
+  startx=e.offsetX-inner_slider.offsetLeft;
+  outerContainer.style.cursor="grabbing";
+})
+
+outerContainer.addEventListener('mouseenter',(e)=>{
+  outerContainer.style.cursor='grab';
+})
+
+
+outerContainer.addEventListener('mouseup',(e)=>{
+  outerContainer.style.cursor='grab';
+})
+
+window.addEventListener('mouseup',()=>{
+  pressed=false;
+})
+
+// console.log("hhh");
+outerContainer.addEventListener('mousemove',(e)=>{
+  if(!pressed)return;
+
+  e.preventDefault();
+
+  x=e.offsetX;
+  console.log("this is e.offetX",x);
+  inner_slider.style.left=`${x-startx}px`;
+  console.log("hhh");
+  console.log(inner_slider.style.left);
+   checkboundry();
+})
+
+
+
+
+
 window.onscroll = function () {
     myFunction();
   };
@@ -11,3 +57,16 @@ window.onscroll = function () {
         .classList.remove("onscroll");
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+  
